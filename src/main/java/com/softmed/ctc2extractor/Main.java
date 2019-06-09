9dcb6e1a01a8d091d1ffa4dbde1abf341178e202
cozej4 <ilakozejumanne@gmail.com>
2019-06-09 11:40:47 +0300

    Added export to excel button

--- src/main/java/com/softmed/ctc2extractor/Main.java
+++ src/main/java/com/softmed/ctc2extractor/Main.java
@@ -111,16 +111,34 @@ public class Main {
 
         label4.setAlignmentX(Component.LEFT_ALIGNMENT);
         JButton button = new JButton();
-        button.setText("Start Data Synchronization");
+        button.setText("Export to excel");
 
         button.setAlignmentX(Component.LEFT_ALIGNMENT);
-
         button.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 Runnable r = new Runnable() {
                     public void run() {
                         if (CTC2DatabaseLocation != null && !hfrCode.equals(""))
-                            ObtainDataFromCTC2(CTC2DatabaseLocation);
+                            ObtainDataFromCTC2(CTC2DatabaseLocation,"export");
+                        else {
+                            log.append("\n\n Please select the correct CTC2 Database location");
+                        }
+                    }
+                };
+                new Thread(r).start();
+            }
+        });
+
+
+        JButton button2 = new JButton();
+        button2.setText("Synchronize data");
+        button2.setAlignmentX(Component.LEFT_ALIGNMENT);
+        button2.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                Runnable r = new Runnable() {
+                    public void run() {
+                        if (CTC2DatabaseLocation != null && !hfrCode.equals(""))
+                            ObtainDataFromCTC2(CTC2DatabaseLocation,"sync");
                         else {
                             log.append("\n\n Please select the correct CTC2 Database location");
                         }
@@ -153,11 +171,12 @@ public class Main {
         panel.add(label3);
         panel.add(label4);
         panel.add(button);
+        panel.add(button2);
         panel.add(scroll);
 
         frame.setJMenuBar(menuBar);
         frame.add(panel);
-        frame.setSize(410, 310);
+        frame.setSize(410, 340);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setVisible(true);
@@ -267,7 +286,7 @@ public class Main {
 
     }
 
-    public static void ObtainDataFromCTC2(String fileLocation) {
+    public static void ObtainDataFromCTC2(String fileLocation,String state) {
         int numberOfPatientsWithMissedAppointments = 0;
         try {
             System.out.println("CTC DATABASE Location = " + fileLocation);
@@ -529,20 +548,24 @@ public class Main {
         }
 
         ctcPatientsModel.setCtcPatientsDTOS(ctcPatients);
-        Gson gson = new Gson();
-        String json = gson.toJson(ctcPatientsModel);
 
-        HttpClient httpClient = new DefaultHttpClient();
-
-        System.out.println("Patients data = " + json);
         System.out.println("Patients found = " + count);
-
         log.append("\n\nPatients with LTFs found = : " + count);
 
         generateExcel(ctcPatients);
+        if(state.equalsIgnoreCase("sync")){
+            syncData(ctcPatientsModel);
+        }
+
+
+
+    }
+
+    public static void syncData( CTCPatientsModel ctcPatientsModel){
         System.out.println("Sending data to server");
         log.append("\n\nSending data to server");
-
+        String json = new Gson().toJson(ctcPatientsModel);
+        HttpClient httpClient = new DefaultHttpClient();
         String username = "username";
         String password = "password";
 
@@ -573,10 +596,12 @@ public class Main {
         } finally {
             httpClient.getConnectionManager().shutdown();
         }
-
     }
 
     public static void generateExcel(List<CTCPatient> ctcPatients){
+
+        System.out.println("Generating EXCEL export");
+        log.append("\n\nGenerating EXCEL export");
         //Blank workbook
         XSSFWorkbook workbook = new XSSFWorkbook();
 
