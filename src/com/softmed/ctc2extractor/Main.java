87d1fb70dcf7d6cd2a9c91b58939423fb0aa73b1
cozej4 <ilakozejumanne@gmail.com>
2019-08-13 05:06:30 +0300

    Code refactoring

--- src/com/softmed/ctc2extractor/Main.java
+++ src/com/softmed/ctc2extractor/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         System.out.println("");
         Parent root = FXMLLoader.load(getClass().getResource("/sample.fxml"));
-        primaryStage.setTitle("Hello World");
+        primaryStage.setTitle("CTC2 Data Extractor");
         primaryStage.setScene(new Scene(root, 470, 469));
         primaryStage.show();
     }
